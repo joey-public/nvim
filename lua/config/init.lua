f65cef2beaea91a9d7a219af49bf4c11c2a442e1
joey-public <joeypublicgit@gmail.com>
2024-06-22 09:23:35 -0700

    updated a few basic commands

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -23,7 +23,7 @@ local opts = {
 			"gzip",
 			"matchit",
 			"matchparen",
-			"netrwPlugin",
+--			"netrwPlugin",
 			"tarPlugin",
 			"tohtml",
 			"tutor", 
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,15 @@
 local keymap = vim.keymap
 
+--set(<mode>, <mycmd>, <defaultcmd>, {desc})
+--    mode: "i", "n", "" (insert, normal, all)
+--    mycmd: the shortcut you want to use
+--    defaultcmd: the command you want to trigger
+--    desc: and optional description 
 keymap.set("", "<leader><leader>", "<S-:>", {desc = "Enter command"})
 keymap.set("n", "oo", "o<esc>", {desc = "goto nexline"})
+keymap.set("n", "<leader>f", 
+                "<S-:>e .<CR>", 
+                {desc = "open file explorer in current window"})
+keymap.set("n", "<leader>ff", 
+                "<S-:>let g:netrw_banner=0 | split | e .  <CR>", 
+                {desc = "open file explorer in new window"})
